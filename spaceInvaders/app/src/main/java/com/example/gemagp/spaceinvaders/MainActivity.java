418cac652acc43b7d2e1653eb47190b445f628cb
Segismundo <kaoss176@gmail.com>
2019-05-30 19:55:50 +0200

    Commit inicial

---
+++ spaceInvaders/app/src/main/java/com/example/gemagp/spaceinvaders/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.gemagp.spaceinvaders;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int INTERVALO = 2000; // = a 2 segundos para salir
+    private long tiempoPrimerClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void accion1(View v){
+        Intent i = new Intent(this, Juego.class);
+        startActivity(i);
+    }
+    public void AcercaDe(View v){
+        Intent i = new Intent(this, AcercaDeActivity.class);
+        startActivity(i);
+    }
+    public void Puntajes(View v){
+        Intent i = new Intent(this, PuntuacionesActivity.class);
+        startActivity(i);
+    }
+    @Override
+    public void onBackPressed(){
+        if (tiempoPrimerClick + INTERVALO > System.currentTimeMillis()){
+            super.onBackPressed();
+            return;
+        }else {
+            Toast.makeText(this, "Presiona 2 veces para salir", Toast.LENGTH_SHORT).show();
+        }
+        tiempoPrimerClick = System.currentTimeMillis();
+    }
+
+    private class AcercaDeActivity {
+    }
+}
---
+++ spaceInvaders/app/src/main/java/com/example/gemagp/spaceinvaders/PuntuacionesActivity.java
@@ -0,0 +1,12 @@
+package com.example.gemagp.spaceinvaders;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class PuntuacionesActivity extends ListActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.puntuaciones);
+    }
+}
