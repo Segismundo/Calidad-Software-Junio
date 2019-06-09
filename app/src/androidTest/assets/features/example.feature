b4f68eb9776793a4e802d16b3d964d3584d282ed
Segismundo <kaoss176@gmail.com>
2019-06-09 14:44:28 +0200

    Cucumber Example

---
+++ app/src/androidTest/assets/features/example.feature
@@ -0,0 +1,8 @@
+Feature: UH from SpaceInvaders application
+
+Scenario: safadas
+  Given asdasda
+  And asfsadsf
+  When asdsfaf
+  Then asfsadsd
+  But assdsadasd
---
+++ app/src/androidTest/java/spaceInvaders/MyStepdefs.java
@@ -0,0 +1,27 @@
+package spaceInvaders;
+
+import cucumber.api.java.en.*;
+
+class MyStepdefs {
+    @Given("^asdasda$")
+    public void asdasda() {
+    }
+
+    @And("^asfsadsf$")
+    public void asfsadsf() {
+    }
+
+    @When("^asdsfaf$")
+    public void asdsfaf() {
+        
+    }
+
+    @Then("^asfsadsd$")
+    public void asfsadsd() {
+        
+    }
+
+    @But("^assdsadasd$")
+    public void assdsadasd() {
+    }
+}
