419b92bcc430dd7646a74fe1957513aaaebe7f3b
Segismundo <kaoss176@gmail.com>
2019-06-18 13:14:09 +0200

    Appium y 5 HU

--- app/src/androidTest/assets/features/UserStories.feature
+++ app/src/androidTest/assets/features/UserStories.feature
@@ -1,13 +1,30 @@
 Feature: UH from SpaceInvaders application
 
   Background:
-    Given We enter the game
+    Given We open the app
 
-  Scenario: Complete a good game
-    Given We make a good score
-    When We enter the name "ExampleName"
-    Then We see our ranking in the game
 
-  Scenario: Complete a bad game
-    Given We make a bad score
-    Then We don't see our ranking in the game
+  Scenario: Open a game
+    When We start the game
+    Then We see an activity Screen
+
+  Scenario: Kill an alien
+    Given We start the game
+    When We shoot an alien
+    Then The alien die
+
+  Scenario: Hit an alien and get points
+    Given We start the game
+    When We shoot an alien
+    Then We score some points
+
+  Scenario: We should be able to exit the game
+    Given We start the game
+    When We press back button
+    Then We should exit the game
+
+  Scenario: We complete a game
+    Given We start the game
+    When Die in the game
+    Then We enter the name "<name>"
+    And We see our ranking in the game
--- app/src/androidTest/java/spaceinvaders/test/UserStoriesDef.java
+++ app/src/androidTest/java/spaceinvaders/test/UserStoriesDef.java
@@ -1,40 +1,76 @@
 package spaceinvaders.test;
 
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import spaceinvaders.MainActivity;
+
+import static org.junit.Assert.assertNotNull;
 
 public class UserStoriesDef {
 
-    @Given("^We enter the game$")
-    public void weEnterTheGame() {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    private Activity activity;
+
+    @Given("^We open the app$")
+    public void weOpenTheApp() {
+        assertNotNull(activity);
+    }
+
+    @When("^We start the game$")
+    public void weStartTheGame() {
+    }
+
+    @Then("^We see an activity Screen$")
+    public void weSeeAnActivityScreen() {
+    }
+
+    @When("^We shoot an alien$")
+    public void weShootAnAlien() {
 
     }
 
-    @Given("^We make a good score$")
-    public void weMakeAGoodScore() {
+    @Then("^The alien die$")
+    public void theAlienDie() {
 
     }
 
-    @When("^We enter the name \"([^\"]*)\"$")
+    @Then("^We score some points$")
+    public void weScoreSomePoints() {
+
+    }
+
+    @When("^We press back button$")
+    public void wePressBackButton() {
+
+    }
+
+    @Then("^We should exit the game$")
+    public void weShouldExitTheGame() {
+
+    }
+
+    @When("^Die in the game$")
+    public void dieInTheGame() {
+
+    }
+
+    @Then("^We enter the name \"([^\"]*)\"$")
     public void weEnterTheName(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
-    @Then("^We see our ranking in the game$")
+    @And("^We see our ranking in the game$")
     public void weSeeOurRankingInTheGame() {
-
-    }
-
-    @Given("^We make a bad score$")
-    public void weMakeABadScore() {
-
-    }
-
-    @Then("^We don't see our ranking in the game$")
-    public void weDonTSeeOurRankingInTheGame() {
-
     }
 }
