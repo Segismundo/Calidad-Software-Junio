fc3131b354d7c04fc9c61ee53759cbcbd6f176e4
Segismundo <kaoss176@gmail.com>
2019-06-20 09:46:38 +0200

    update smoke test

--- app/src/test/java/spaceinvaders/utils/SmokeTest.kt
+++ app/src/test/java/spaceinvaders/utils/SmokeTest.kt
@@ -2,9 +2,6 @@ package spaceinvaders.utils
 
 import java.lang.annotation.Inherited
 
-/**
- * Created by elysia.lock on 4/17/18.
- */
 
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 @Inherited
